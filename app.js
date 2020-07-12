3cd5224340c100e9f3bf0e237bf4249ebfa4b21e
hungry-eater <didwogns789@naver.com>
2020-07-12 15:54:37 +0900

    Register and Session

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import dotenv from "dotenv";
+dotenv.config();
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import passport from "passport";
 import { localsMiddelwares } from "./middlewares";
+import "./passport";
 
 const app = express();
 
@@ -19,6 +24,17 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddelwares);
 
 app.use(routes.users, userRouter);
